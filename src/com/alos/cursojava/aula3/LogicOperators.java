41dba43ed5c34113527ff2a98cc47c628666970f
4lexRossi <devalexrossi@gmail.com>
2021-01-02 17:57:17 -0300

    Operadores Lógicos

---
+++ src/com/alos/cursojava/aula3/LogicOperators.java
@@ -0,0 +1,17 @@
+package com.alos.cursojava.aula3;
+
+public class LogicOperators {
+
+	public static void main(String[] args) {
+		int value1 = 1;
+		 int value2 = 2;
+		 
+		 boolean result = (value1 == 1) && (value2 == 2);
+		 System.out.println("Value 1 is 1 AND Value 2 is 2: " + result);
+		 
+		 boolean result2 = (value1 == 1) || (value2 == 2);
+		 System.out.println("Value 1 is 1 OR Value 2 is 2: " + result2);
+		 
+	}
+
+}
