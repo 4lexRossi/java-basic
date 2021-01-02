4bb1d63b176b9d734bde99746515cd9bfdb8b314
4lexRossi <devalexrossi@gmail.com>
2021-01-02 17:43:17 -0300

    Operadores Aritméticos

---
+++ src/com/alos/cursojava/aula3/AritmeticOperators.java
@@ -0,0 +1,37 @@
+package com.alos.cursojava.aula3;
+
+public class AritmeticOperators {
+
+	public static void main(String[] args) {
+		
+		int result = 1 + 2;
+		System.out.println(result);
+		
+		result = result -1;
+		System.out.println(result);
+		
+		result = result * 2;
+		System.out.println(result);
+		
+		result = result / 2;
+		System.out.println(result);
+		
+		result = result + 8;
+		System.out.println(result);
+		
+		result = result % 7;
+		System.out.println(result);
+		
+		result++;
+		System.out.println(result);		
+		
+		System.out.println(result++);
+		System.out.println(result);
+		System.out.println(++result);
+		
+		System.out.println(result--);
+		System.out.println(result);
+		System.out.println(--result);
+	}
+
+}
