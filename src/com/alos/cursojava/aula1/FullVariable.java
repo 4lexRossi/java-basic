80770798adb171670c1d9aada7f1fb104e4e4d95
4lexRossi <devalexrossi@gmail.com>
2021-01-02 14:27:11 -0300

    Aulas introdução, 1 e 2

---
+++ src/com/alos/cursojava/aula1/FullVariable.java
@@ -0,0 +1,16 @@
+package com.alos.cursojava.aula1;
+
+public class FullVariable {
+
+	public static void main(String[] args) {
+		byte age01 = 20;
+		short age02 = 21;
+		int age03 = 22;
+		long age04 = 23;
+		
+		System.out.println(age01);
+		System.out.println(age02);
+		System.out.println(age03);
+		System.out.println(age04);	
+	}
+}
